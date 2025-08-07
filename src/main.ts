2226d584a2bcfa17b7e000fa34688a6ab725f341
Leya <rainandsoup@gmail.com>
2025-08-07 21:24:07 +0300

    chore: установка bootstrap и pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia).mount("#app");
