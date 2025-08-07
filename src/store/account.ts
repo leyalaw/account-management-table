4903873ebb7236ef565ca588b0249db8eaf67341
Leya <rainandsoup@gmail.com>
2025-08-07 23:27:55 +0300

    feat: создание хранилища учётных записей

---
+++ src/store/account.ts
@@ -0,0 +1,67 @@
+// типы
+import type * as Account from "../types/account";
+// основное
+import { ref, watch } from "vue";
+import { defineStore } from "pinia";
+
+/* -------------------------------------------------------------------------- */
+/*                         Хранилище учётных записей                         */
+/* -------------------------------------------------------------------------- */
+
+export const useAccountStore = defineStore("account", () => {
+  /** Получение учётных записей из localStorage */
+  const getAccounts = () => {
+    const storedAccounts = localStorage.getItem("accounts");
+    return storedAccounts ? JSON.parse(storedAccounts) : [];
+  };
+
+  /** Список учётных записей */
+  const accounts = ref<Account.Data[]>(getAccounts());
+  /** Типы учётных записей */
+  const accountTypes: Account.Type[] = [
+    { value: "ldap", label: "LDAP" },
+    { value: "local", label: "Локальная" },
+  ];
+
+  // сохранение в localStorage
+  watch(
+    accounts,
+    () => localStorage.setItem("accounts", JSON.stringify(accounts.value)),
+    { deep: true }
+  );
+
+  /** Получение "пустой" учётной записи */
+  const getDummyAccount = (): Account.LdapData => ({
+    id: Date.now().toString(),
+    tags: [],
+    login: "",
+    type: "ldap",
+    password: null,
+  });
+
+  /** Добавление учётной записи */
+  const addAccount = (data: Account.Data) => accounts.value.push(data);
+
+  /** Редактирование учётной записи */
+  const updateAccount = (data: Account.Data) => {
+    const index = accounts.value.findIndex((account) => account.id === data.id);
+
+    if (index < 0) addAccount(data);
+    else accounts.value[index] = data;
+  };
+
+  /** Удаление учётных записей */
+  const removeAccounts = (...ids: Account.Id[]) =>
+    (accounts.value = accounts.value.filter(
+      (account) => !ids.includes(account.id)
+    ));
+
+  return {
+    accounts,
+    accountTypes,
+    getDummyAccount,
+    addAccount,
+    updateAccount,
+    removeAccounts,
+  };
+});
