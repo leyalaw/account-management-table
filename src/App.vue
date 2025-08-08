b3366cf402e46d381b9719e0a5d3a66efadc1b11
Leya <rainandsoup@gmail.com>
2025-08-08 15:07:55 +0300

    feat: оформление компонента AccountManagementTable в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<template></template>
+<template>
+  <main class="container my-4">
+    <header class="mb-4">
+      <h1>Управление учётными записями</h1>
+    </header>
+    <AccountManagementTable />
+  </main>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AccountManagementTable from "@components/AccountManagementTable.vue";
+</script>
 
 <style scoped></style>
